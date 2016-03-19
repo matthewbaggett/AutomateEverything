a71b1c85dbb81a7d984db55c0a6c7c7f620f2c42
Matthew Baggett <matthew@baggett.me>
2016-03-19 23:18:50 +0000

    Still got that issue..

--- monitors/ip-camera/monitor.php
+++ monitors/ip-camera/monitor.php
@@ -5,18 +5,17 @@ require_once("vendor/autoload.php");
 $environment = array_merge($_SERVER, $_ENV);
 ksort($environment);
 
-\Kint::dump($environment);
-
 $cameras = [];
 
 foreach($environment as $key => $value){
     if(substr($key, 0, 7) == 'CAMERA_'){
         $elements = explode("_", $key);
         $cameras[$elements[1]][$elements[2]] = $value;
+        $cameras[$elements[1]]['NAME'] = $elements[1];
     }
-    $cameras[$elements[1]]['NAME'] = $elements[1];
 }
 unset($cameras['']);
+echo "\nStarting monitors...\n";
 
 while($camera = array_pop($cameras)) {
     $pid = pcntl_fork();
@@ -24,16 +23,15 @@ while($camera = array_pop($cameras)) {
         die ("Could not fork :(\n");
     } else if ($pid) {
         // parent
+        continue;
     } else {
         // child
-        \Kint::dump($camera);exit;
         $name = strtolower($camera['NAME']);
         $host = $camera['HOST'];
         $port = $camera['PORT'];
         $auth = $camera['AUTH'];
         $mediapath = $camera['MEDIAPATH'];
         echo " > Camera: {$name}\n";
-
 
         $ffmpeg_command = "ffmpeg -i rtsp://{$auth}@{$host}:{$port}{$mediapath} -c copy -map 0 -acodec mp2 -f segment -strftime 1 -segment_time 60 -segment_format mp4 {$name}_%Y-%m-%d_%H-%M-%S.mp4";
         //passthru($ffmpeg_command);
