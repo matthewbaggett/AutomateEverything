371bc70ab78f974d716b625ce84658196cf839de
Matthew Baggett <matthew@baggett.me>
2016-03-19 23:08:47 +0000

    Camera actually connects. Threading issue tho..

--- monitors/ip-camera/monitor.php
+++ monitors/ip-camera/monitor.php
@@ -14,24 +14,26 @@ foreach($environment as $key => $value){
         $elements = explode("_", $key);
         $cameras[$elements[1]][$elements[2]] = $value;
     }
+    $cameras[$elements[1]]['NAME'] = $elements[1];
 }
 
 \Kint::dump($cameras);
 
-foreach($cameras as $camera_name => $camera){
+while($camera = array_pop($cameras)) {
     $pid = pcntl_fork();
-    if($pid == -1){
+    if ($pid == -1) {
         die ("Could not fork :(\n");
-    }else if ($pid){
+    } else if ($pid) {
         // parent
-    }else{
+    } else {
         // child
-        echo " > Camera: {$camera_name}\n";
-        $name = strtolower($camera_name);
+        $name = strtolower($camera['NAME']);
         $host = $camera['HOST'];
         $port = $camera['PORT'];
         $auth = $camera['AUTH'];
         $mediapath = $camera['MEDIAPATH'];
+        echo " > Camera: {$name}\n";
+
 
         $ffmpeg_command = "ffmpeg -i rtsp://{$auth}@{$host}:{$port}{$mediapath} -c copy -map 0 -acodec mp2 -f segment -strftime 1 -segment_time 60 -segment_format mp4 {$name}_%Y-%m-%d_%H-%M-%S.mp4";
         //passthru($ffmpeg_command);
